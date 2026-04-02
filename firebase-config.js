626394b682168033ac4c114d7d7552a2efcb754a
marialhonorato <maria@prismasaude.com.br>
2026-04-02 19:38:27 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,12 +7,12 @@
    ============================================ */
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAPjCilzKwn9N8gJGACzBAd06hkm_GRE9I",
-    authDomain: "prisma-clinica.firebaseapp.com",
-    projectId: "prisma-clinica",
-    storageBucket: "prisma-clinica.firebasestorage.app",
-    messagingSenderId: "839487617014",
-    appId: "1:839487617014:web:1920bfa9f0b6eb550d79d7"
+    apiKey: "AIzaSyCthfBOmNUU_vOp-eSKol4wXZkClT-UtwM",
+    authDomain: "prisma-clinica-ae257.firebaseapp.com",
+    projectId: "prisma-clinica-ae257",
+    storageBucket: "prisma-clinica-ae257.firebasestorage.app",
+    messagingSenderId: "100140286581",
+    appId: "1:100140286581:web:316d4318cde38d7c986e3c"
 };
 
 // Inicializar Firebase
